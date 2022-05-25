08ff2966bdc34d5c214f549c81a09054bae5c4c6
PatrykPat <106230641+PatrykPat@users.noreply.github.com>
2022-05-25 09:32:11 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+$host = '';
+$db = 'inloggenKras';
+$user = 'root';
+$password = '';
+$patryk = "UTF8";
+$dsn = "mysql:host=$host;dbname=$db;charset=$patryk";
+
+$options = [
+    PDO::ATTR_ERRMODE                          => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE               => PDO::FETCH_CLASS,
+    PDO::ATTR_EMULATE_PREPARES                 => false,
+    ];
+    
+try {
+    $pdo = new PDO ($dsn, $user, $password, $options);
+    } 
+catch (\PDOException $e){
+        throw new \PDOException($e->getMessage(), (int)$e->getCode ());
+    }
+ 
+if($pdo === false){
+    die("ERROR: Could not connect. " . $pdo->connect_error);
+}
+?>
