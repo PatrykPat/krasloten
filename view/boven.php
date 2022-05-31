ad1043859d469b2e97ba84592eddb8f848895d37
CritHim <105359408+CritHim@users.noreply.github.com>
2022-05-31 10:02:45 +0200

    Add files via upload

---
+++ view/boven.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <title>Logboek Project</title>
+</head>
